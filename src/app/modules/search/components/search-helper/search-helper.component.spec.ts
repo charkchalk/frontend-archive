2149aaa72964157ebeb4833a7d20ffa6b598a3ff
moontai0724 <moontai0724@gmail.com>
2022-12-19 17:48:50 +0800

    test: search-helper

--- src/app/modules/search/components/search-helper/search-helper.component.spec.ts
+++ src/app/modules/search/components/search-helper/search-helper.component.spec.ts
@@ -1,23 +1,33 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatAutocompleteModule } from "@angular/material/autocomplete";
+import { MatChipsModule } from "@angular/material/chips";
 import { MatIconModule } from "@angular/material/icon";
-import { MatLegacyAutocompleteModule as MatAutocompleteModule } from "@angular/material/legacy-autocomplete";
-import { MatLegacyChipsModule as MatChipsModule } from "@angular/material/legacy-chips";
-import { MatLegacySelectModule as MatSelectModule } from "@angular/material/legacy-select";
+import { MatSelectModule } from "@angular/material/select";
 import { By } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { SearchService } from "../../search.service";
+import { SelectContentInputComponent } from "../search-helper-item/content-input/select-content-input/select-content-input.component";
+import { TestSelectFilter } from "../search-helper-item/content-input/select-content-input/select-content-input.component.spec";
+import { TextContentInputComponent } from "../search-helper-item/content-input/text-content-input/text-content-input.component";
 import { SearchHelperItemComponent } from "../search-helper-item/search-helper-item.component";
 import { SearchHelperComponent } from "./search-helper.component";
 
 describe("SearchHelperComponent", () => {
   let component: SearchHelperComponent;
   let fixture: ComponentFixture<SearchHelperComponent>;
+  let searchServiceSpy: jasmine.SpyObj<SearchService>;
 
   beforeEach(async () => {
+    const spy = new SearchService();
     await TestBed.configureTestingModule({
-      declarations: [SearchHelperComponent, SearchHelperItemComponent],
+      declarations: [
+        SearchHelperComponent,
+        SearchHelperItemComponent,
+        SelectContentInputComponent,
+        TextContentInputComponent,
+      ],
       imports: [
         BrowserAnimationsModule,
         FormsModule,
@@ -27,10 +37,13 @@ describe("SearchHelperComponent", () => {
         MatIconModule,
         MatSelectModule,
       ],
-      providers: [{ provide: SearchService }],
+      providers: [{ provide: SearchService, useValue: spy }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SearchHelperComponent);
+    searchServiceSpy = TestBed.inject(
+      SearchService,
+    ) as jasmine.SpyObj<SearchService>;
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -40,10 +53,42 @@ describe("SearchHelperComponent", () => {
   });
 
   it("should have a default empty filter", () => {
-    const searchHelperItems = fixture.nativeElement.querySelectorAll(
-      "app-search-helper-item",
+    const searchHelperItems = fixture.debugElement.queryAll(
+      By.directive(SearchHelperItemComponent),
     );
     expect(searchHelperItems.length).toEqual(1);
+  });
+
+  it("should replace a filter when event emits from item", async () => {
+    const firstItem: SearchHelperItemComponent = fixture.debugElement.query(
+      By.directive(SearchHelperItemComponent),
+    ).componentInstance;
+    searchServiceSpy.getFilter = jasmine
+      .createSpy()
+      .and.returnValue(TestSelectFilter);
+    firstItem.changed.emit(TestSelectFilter.key);
+
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    const changedItem: SearchHelperItemComponent = fixture.debugElement.query(
+      By.directive(SearchHelperItemComponent),
+    ).componentInstance;
+    expect(changedItem.filter?.getKey()).toBe(TestSelectFilter.key);
+  });
+
+  it("should remove a filter when event emits from item", () => {
+    const firstItem: SearchHelperItemComponent = fixture.debugElement.query(
+      By.directive(SearchHelperItemComponent),
+    ).componentInstance;
+    firstItem.removed.emit();
+
+    fixture.detectChanges();
+
+    const searchHelperItems = fixture.debugElement.queryAll(
+      By.directive(SearchHelperItemComponent),
+    );
+    expect(searchHelperItems.length).toBe(0);
   });
 
   describe("add filter button", () => {
@@ -55,26 +100,10 @@ describe("SearchHelperComponent", () => {
 
       fixture.detectChanges();
 
-      const searchHelperItems =
-        fixture.debugElement.nativeElement.querySelectorAll(
-          "app-search-helper-item",
-        );
+      const searchHelperItems = fixture.debugElement.queryAll(
+        By.directive(SearchHelperItemComponent),
+      );
       expect(searchHelperItems.length).toBe(2);
     });
-  });
-
-  it("should remove a filter when event emits from item", () => {
-    const firstItem = fixture.debugElement.query(
-      By.css("app-search-helper-item"),
-    ).componentInstance as SearchHelperItemComponent;
-    firstItem.removed.emit();
-
-    fixture.detectChanges();
-
-    const searchHelperItems =
-      fixture.debugElement.nativeElement.querySelectorAll(
-        "app-search-helper-item",
-      );
-    expect(searchHelperItems.length).toBe(0);
   });
 });
